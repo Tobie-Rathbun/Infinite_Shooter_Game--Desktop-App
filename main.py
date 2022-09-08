9604f0e8816696c447d2837a2a6a4073eb980042
Tobie Rathbun <tsrathbun@gmail.com>
2022-09-08 15:46:43 -0700

    implementation of raycasting

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 from settings import *
 from map import *
 from player import *
-
+from raycasting import *
 
 #game constructor class
 class Game:
@@ -18,9 +18,11 @@ class Game:
     def new_game(self):
         self.map = Map(self)
         self.player = Player(self)
+        self.raycasting = RayCasting(self)
 
     def update(self):
         self.player.update()
+        self.raycasting.update()
         pg.display.flip()
         self.delta_time = self.clock.tick(FPS)
         pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
