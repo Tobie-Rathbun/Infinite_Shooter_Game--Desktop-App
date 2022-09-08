29c3e94dbc84bc0dc43ade676520f22b75756115
Tobie Rathbun <tsrathbun@gmail.com>
2022-09-08 12:41:01 -0700

    first commit

---
+++ settings.py
@@ -0,0 +1,10 @@
+# /// Settings for the Game ///
+
+#screen variables
+RES = WIDTH, HEIGHT = 1600, 900
+FPS = 0
+
+PLAYER_POS = 1.5, 5
+PLAYER_ANGLE = 0
+PLAYER_SPEED = 0.004
+PLAYER_ROT_SPEED = 0.002
