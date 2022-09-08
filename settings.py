854d182d15b8e5dbd8b0e38d0d54fed620a881a6
Tobie Rathbun <tsrathbun@gmail.com>
2022-09-08 13:01:15 -0700

    settings update

--- settings.py
+++ settings.py
@@ -1,3 +1,4 @@
+import math
 # /// Settings for the Game ///
 
 #screen variables
@@ -8,3 +9,10 @@ PLAYER_POS = 1.5, 5
 PLAYER_ANGLE = 0
 PLAYER_SPEED = 0.004
 PLAYER_ROT_SPEED = 0.002
+
+FOV = math.pi / 3
+HALF_FOV = FOV / 2
+NUM_RAYS = WIDTH // 2
+HALF_NUM_RAYS = NUM_RAYS // 2
+DELTA_ANGLE = FOV / NUM_RAYS
+MAX_DEPTH = 20
