38834a71f74663586a9dc9839e6a93d910870f33
Tobie Rathbun <tsrathbun@gmail.com>
2022-09-08 13:28:33 -0700

    depth, trig

--- raycasting.py
+++ raycasting.py
@@ -7,7 +7,32 @@ class RayCasting:
         self.game = game
 
     def ray_cast(self):
-        pass
+        ox, oy = self.game.player.pos
+        x_map, y_map = self.game.player.map_pos
+
+        ray_angle = self.game.player.angle - HALF_FOV + 0.0001
+        for ray in range(NUM_RAYS):
+            sin_a = math.sin(ray_angle)
+            cos_a = math.cos(ray_angle)
+
+            #vericals
+            x_vert, dx = (x_map + 1, 1) if cos_a > 0 else (x_map - 1e-6, -1)
+
+            depth_vert = (x_vert - ox) / cos_a
+            y_vert = oy + depth_vert * sin_a
+
+            delta_depth = dx / cos_a
+            dy = delta_depth * sin_a
+
+            for i in range(MAX_DEPTH):
+                tile_vert = int(x_vert), int(y_vert)
+                if tile_vert in self.game.map.world_map:
+                    break
+                x_vert += dx
+                y_vert += dy
+                depth_vert += delta_depth
+
+            ray_angle += DELTA_ANGLE 
 
     def update(self):
         self.ray_cast()
