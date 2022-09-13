4b83dbd6489af20ebf1ee7613f49d1870730857f
Tobie Rathbun <tsrathbun@gmail.com>
2022-09-12 17:28:39 -0700

    added sky texture box

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ class Game:
         pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
 
     def draw(self):
-        self.screen.fill('black')
+        #self.screen.fill('black')
         self.object_renderer.draw()
         #self.map.draw()
         #self.player.draw()
