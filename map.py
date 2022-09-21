3efcd71b2ce31f4c4043af1e74f7cdcb00c33712
Tobie Rathbun <tsrathbun@gmail.com>
2022-09-21 10:01:00 -0700

    testing an exit

--- map.py
+++ map.py
@@ -1,4 +1,5 @@
 import pygame as pg
+from collections import deque
 
 _ = False
 mini_map = [
@@ -22,7 +23,7 @@ mini_map = [
     [1, _, _, 1, _, _, _, _, _, _, 2, _, _, _, _, 1],
     [1, _, _, 1, _, _, _, _, _, _, 3, _, 1, 1, 1, 1],
     [1, _, _, _, _, _, _, _, _, _, 2, _, _, _, _, 1],
-    [1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 1],
+    [1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 5],
     [1, _, _, _, _, _, _, _, _, _, 1, _, _, _, _, 1],
     [1, 1, 2, 3, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ]
@@ -31,16 +32,26 @@ class Map:
     def __init__(self, game):
         self.game = game
         self.mini_map = mini_map
+        #self.dq = deque()
+        #self.portal = [2, 3, 4, 5]
+        #for num in self.portal:
+        #    self.dq.append(num)
+        #self.f = self.dq[0]
         self.world_map = {}
         self.get_map()
+        
 
     def get_map(self):
         for j, row in enumerate(self.mini_map):
             for i, value in enumerate(row):
                 if value:
+                    #if value == 5:
+                    #    self.world_map[(i, j)] = self.f
                     self.world_map[(i, j)] = value
 
     def draw(self):
+        #self.dq.rotate(-1)
+        #self.f = self.dq[0]
         [pg.draw.rect(self.game.screen, 'darkgray', (pos[0] * 100, pos[1] * 100, 100, 100), 2)
         for pos in self.world_map]
 
