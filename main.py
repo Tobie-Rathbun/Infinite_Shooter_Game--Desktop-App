569b7dd42b1fe70bcd8325193da26670b1f17735
Tobie Rathbun <tsrathbun@gmail.com>
2022-09-22 19:10:26 -0700

    fixed end game to next game

--- main.py
+++ main.py
@@ -85,17 +85,21 @@ class Game:
             self.draw()
 
     def pre_game(self):
+        self.game_running = False
+        self.new_game()
         self.basicFont = pg.font.SysFont(None, 48)
         self.screen.fill(BLACK)
         self.text = self.basicFont.render("WSAD to walk, click to shoot!", True, WHITE)
-        self.text2 = self.basicFont.render("Press SPACE to play!", True, WHITE)
-        self.textRect, self.textRect2 = self.text.get_rect(), self.text2.get_rect()
-        self.textRect.centerx, self.textRect2.centerx = self.screen.get_rect().centerx, self.screen.get_rect().centerx
-        self.textRect.centery, self.textRect2.centery = self.screen.get_rect().centery / 3, self.screen.get_rect().centery / 1.5 
+        self.text2 = self.basicFont.render("Get to the mushroom to make it to the next round", True, WHITE)
+        self.text3 = self.basicFont.render("Press SPACE to play!", True, WHITE)
+        self.textRect, self.textRect2, self.textRect3 = self.text.get_rect(), self.text2.get_rect(), self.text3.get_rect()
+        self.textRect.centerx, self.textRect2.centerx, self.textRect3.centerx = self.screen.get_rect().centerx, self.screen.get_rect().centerx, self.screen.get_rect().centerx
+        self.textRect.centery, self.textRect2.centery, self.textRect3.centery = self.screen.get_rect().centery / 3, self.screen.get_rect().centery / 2, self.screen.get_rect().centery / 1.5 
         self.pregame = True
         while self.pregame:
             self.screen.blit(self.text,self.textRect)
             self.screen.blit(self.text2,self.textRect2)
+            self.screen.blit(self.text3,self.textRect3)
             for event in pg.event.get():
                 if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
                     pg.quit()
@@ -105,13 +109,26 @@ class Game:
             pg.display.flip()
 
     def post_game(self):
-        self.basicFont = pg.font.SysFont(None, 48)
+        self.game_running = False
         self.screen.fill(BLACK)
-        self.text_box_contents = "You made it to round: " + str(self.player.round_number)
-        self.text = self.basicFont.render(self.text_box_contents, True, WHITE)
-        self.textRect = self.text.get_rect()
-        self.textRect.centerx = self.screen.get_rect().centerx
-        self.textRect.centery = self.screen.get_rect().centery / 2
+        self.text = self.basicFont.render("You made it to round: ", True, WHITE)
+        self.text2 = self.basicFont.render(str(self.player.round_number), True, WHITE)
+        self.text3 = self.basicFont.render("Press SPACE to play!", True, WHITE)
+        self.textRect, self.textRect2, self.textRect3 = self.text.get_rect(), self.text2.get_rect(), self.text3.get_rect()
+        self.textRect.centerx, self.textRect2.centerx, self.textRect3.centerx = self.screen.get_rect().centerx, self.screen.get_rect().centerx, self.screen.get_rect().centerx
+        self.textRect.centery, self.textRect2.centery, self.textRect3.centery = self.screen.get_rect().centery / 3, self.screen.get_rect().centery / 2, self.screen.get_rect().centery / 1.5 
+        self.pregame = True
+        while self.pregame:
+            self.screen.blit(self.text,self.textRect)
+            self.screen.blit(self.text2,self.textRect2)
+            self.screen.blit(self.text3,self.textRect3)
+            for event in pg.event.get():
+                if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+                    pg.quit()
+                    sys.exit()
+                elif event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
+                    self.pre_game()
+            pg.display.flip()
 
 if __name__ == '__main__':
     game = Game()
